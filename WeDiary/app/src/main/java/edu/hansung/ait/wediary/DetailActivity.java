a1fe002a51b8db385274757c3eb4484abf65781f
ParkChanMo <opopqkr@naver.com>
2016-12-07 03:45:18 +0900

    eighth

--- WeDiary/app/src/main/java/edu/hansung/ait/wediary/DetailActivity.java
+++ WeDiary/app/src/main/java/edu/hansung/ait/wediary/DetailActivity.java
@@ -14,7 +14,7 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity implements OnItemClickListener, OnClickListener {
+public class DetailActivity extends AppCompatActivity {
     MyDBHelper mDBHelper;
     String today;
     Cursor cursor;
@@ -26,58 +26,6 @@ public class DetailActivity extends AppCompatActivity implements OnItemClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        intent = getIntent();
-        today = intent.getStringExtra("Param1");
 
-        TextView text = (TextView)findViewById(R.id.texttoday);
-        text.setText(today);
-
-        mDBHelper = new MyDBHelper(this, "Today2.db", null, 1);
-        SQLiteDatabase db = mDBHelper.getWritableDatabase();
-
-        cursor = db.rawQuery("SELECT * FROM today WHERE date = '" + today + "'", null);
-
-        adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor,
-                new String[] { "title", "startTime" }, new int[] { android.R.id.text1, android.R.id.text2 });
-
-        ListView list = (ListView)findViewById(R.id.list1);
-        list.setAdapter(adapter);
-        list.setOnItemClickListener(this);
-
-        mDBHelper.close();
-
-        Button btn = (Button) findViewById(R.id.btnadd);
-        btn.setOnClickListener(this);
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(this, EditActivity.class);
-        cursor.moveToPosition(position);
-        intent.putExtra("ParamID", cursor.getInt(0));
-        startActivityForResult(intent, 0);
-    }
-
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(this, EditActivity.class);
-        intent.putExtra("ParamDate", today);
-        startActivityForResult(intent, 1);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch(requestCode) {
-            case 0:
-
-            case 1:
-                if(resultCode == RESULT_OK) {
-                    SQLiteDatabase db = mDBHelper.getWritableDatabase();
-                    cursor = db.rawQuery("SELECT * FROM today WHERE date = '" + today + "'", null);
-                    adapter.changeCursor(cursor);
-                    mDBHelper.close();
-                }
-                break;
-        }
     }
 }
